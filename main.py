42512f80e79a4f3ed9a3dfbed477341bb67dab27
Osminalx <verdestro64@gmail.com>
2024-03-17 16:10:24 -0700

    Poke weaknesses

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ intents.message_content = True  # NOQA
 bot = commands.Bot(command_prefix='$',intents=intents)
 
 #commands
-@bot.command()
+@bot.command(name= 'test', description= 'Just a test command')
 async def test(ctx, arg: str):
     await ctx.send(arg)
 
-@bot.command()
+@bot.command(name='select', description='Se dan varias opciones y se elige una al azár')
 async def select(ctx,*args):
     await ctx.send(f'La suerte eligió {choice(args)}')
 
-@bot.command()
+@bot.command(name='flipc', description='se lanza una moneda y dice si ganó cara o cruz')
 async def flipc(ctx):
     selection = chance_games.flip_a_coin()
     if selection == 1:
@@ -36,7 +36,7 @@ async def flipc(ctx):
     else:
         await ctx.send("Ganó Cruz")
 
-@bot.command()
+@bot.command(name='poke', description= 'Se pasa un nombre o id y se devuelve una tarjeta con información de un pokemon')
 async def poke(ctx,pokemon):
     poke_info = PokemonInfo()
     poke_data = poke_info.get_pokemon_info(pokemon)
@@ -49,12 +49,12 @@ async def poke(ctx,pokemon):
 
     embed.set_image(url=sprite)  
     embed.title = f"{name} # {pkid}"  
-    embed.description = f"Types: {poke_data['types']} \n Abilities: {poke_data['abilities']} \n Weight: {poke_data['weight']}"
+    embed.description = f"Types: {poke_data['types']} \n Abilities: {poke_data['abilities']} \n Weight: {poke_data['weight']} \n Weaknesses: {poke_data['weaknesses']} \n inmune to: {poke_data['inmune_to']}"
 
     await ctx.send(embed=embed)
 
 
-@bot.command()
+@bot.command(name='rmbg', description='Se le pasa una imagen y devolverá la misma imagen sin fondo')
 async def rmbg(ctx,img: Optional[Attachment]):
     if img is None:
         await ctx.send('You did not upload anything!')
--- poke_search.py
+++ poke_search.py
@@ -4,6 +4,21 @@ class PokemonInfo:
     def __init__(self) -> None:
         self.URL = "https://pokeapi.co/api/v2/"
 
+    def _get_type_data(self,type_name:str)->dict:
+        try:
+            response = requests.get(f"{self.URL}type/{type_name}")
+            if response.status_code == 200:
+                type_data = response.json()
+                damage_relations = type_data['damage_relations']
+                double_damage = [damage['name'] for damage in damage_relations['double_damage_from']] 
+                no_damage = [damage['name'] for damage in damage_relations['no_damage_from']]
+                return {
+                    "weak": double_damage,
+                    "no_dmg": no_damage
+                }
+        except Exception as e:
+            return {"error": str(e)}
+
     def get_pokemon_info(self, pk_name: str) -> dict:
         try:
             response = requests.get(f"{self.URL}pokemon/{pk_name}")
@@ -13,13 +28,25 @@ class PokemonInfo:
                 abilities = [ability_data['ability']['name'] for ability_data in pokemon_data['abilities']]
                 sprite = pokemon_data['sprites']['front_default']
 
+                types_data = [self._get_type_data(type_name) for type_name in types]
+
+                # Combinar la información de tipo de daño de todos los tipos
+                weak_types = set()
+                no_damage_types = set()
+                for data in types_data:
+                    if data:
+                        weak_types.update(data.get('weak', []))
+                        no_damage_types.update(data.get('no_dmg', []))
+
                 pokemon_info = {
                     'name': pokemon_data['name'],
                     'id': pokemon_data['id'],
                     'sprite': sprite,
                     'types': types,
                     'abilities': abilities,
-                    'weight': pokemon_data['weight']
+                    'weight': pokemon_data['weight'],
+                    'weaknesses': list(weak_types),
+                    'inmune_to': list(no_damage_types)
                 }
                 return pokemon_info
             else:
@@ -28,6 +55,8 @@ class PokemonInfo:
             return {"error": str(e)}
 
 # Ejemplo de uso
-poke_info = PokemonInfo()
-pokemon_data = poke_info.get_pokemon_info("snivy")
-print(pokemon_data)
+#poke_info = PokemonInfo()
+#pokemon_data = poke_info.get_pokemon_info("tyranitar")
+#print(pokemon_data)
+#type_data = poke_info.get_type_data("electric")
+#print(type_data)
